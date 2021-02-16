fff781c8f1c030c5e51f8a8c76da0bfe59bc350f
Jessica Wilkins <jwilkin4@hotmail.com>
2021-02-16 05:43:14 -0800

    adding landing page components to app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Footer, Home, Shop, About, Contact } from "./components";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/about" exact component={() => <About />} />
+          <Route path="/shop" exact component={() => <Shop />} />
+          <Route path="/contact" exact component={() => <Contact />} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
