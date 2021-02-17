0d22c440c83482444e4867937dce694015997a04
Jessica Wilkins <jwilkin4@hotmail.com>
2021-02-17 03:00:10 -0800

    adding product categories

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 function Shop() {
     return (
         <div>
-            <h1>Shop</h1>
+            <h1 className="shop-title">Shop</h1>
             <div class="row row-cols-1 row-cols-md-2 g-4">
                 <div class="col">
                     <div class="card">
-                        <img src="https://play-guitars.com/wp-content/uploads/2019/03/electric-guitar-for-beginners-01.jpg" alt="guitar" class="card-img-top" />
+                        <img src="https://play-guitars.com/wp-content/uploads/2019/03/electric-guitar-for-beginners-01.jpg" alt="guitar" class="card-img-top product-img" />
                         <div class="card-body">
                             <h2 class="card-title">Guitars</h2>
                             <p class="card-text">Browse</p>
@@ -16,7 +16,16 @@ function Shop() {
                 </div>
                 <div class="col">
                     <div class="card">
-                        <img src="http://thevault.musicarts.com/wp-content/uploads/2020/10/Horns-of-Plenty-DE-47-scaled.jpg" alt="trumpet" class="card-img-top" />
+                        <img src="https://www.musictech.net/wp-content/uploads/2020/03/Bass-Player-Credit-Shutterstock@1400x1050.jpg" alt="bass" class="card-img-top product-img" />
+                        <div class="card-body">
+                            <h2 class="card-title">Basses</h2>
+                            <p class="card-text">Browse</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="https://cdn.quotesgram.com/img/93/74/648440396-Screen-Shot-2013-08-19-at-11_33_19-PM.png" alt="trumpet" class="card-img-top product-img" />
                         <div class="card-body">
                             <h2 class="card-title">Brass</h2>
                             <p class="card-text">Browse</p>
@@ -25,27 +34,91 @@ function Shop() {
                 </div>
                 <div class="col">
                     <div class="card">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROmagvp_SgPnb29gV-FI6netYz9WG2BhVJxg&usqp=CAU" alt="playing clarinet" class="card-img-top" />
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROmagvp_SgPnb29gV-FI6netYz9WG2BhVJxg&usqp=CAU" alt="playing clarinet" class="card-img-top product-img" />
                         <div class="card-body">
                             <h2 class="card-title">Woodwinds</h2>
                             <p class="card-text">Browse</p>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="row row-cols-1 row-cols-md-2 g-4">
                 <div class="col">
                     <div class="card">
-                        <img src="https://images.squarespace-cdn.com/content/v1/5a57dd0c010027ec424db45c/1550796308545-BWVTTW2KJ6H4AWBRY2PA/ke17ZwdGBToddI8pDm48kG87Sfbgg29A4BYEDq3OXvgUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcf4OxbJOyh_wHUnyc4kQLQ6SBshRGOku7c30Y_IRDNPta8R2IY5BHMaEj1zOWoDTZ/photo-1519892300165-cb5542fb47c7.jpg?format=2500w" alt="drumset" class="card-img-top" />
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpiZ3t-vheMPZENrRALvfDVsiMFmWV_NXKCA&usqp=CAU" alt="drumset" class="card-img-top product-img" />
                         <div class="card-body">
                             <h2 class="card-title">Percussion</h2>
                             <p class="card-text">Browse</p>
                         </div>
                     </div>
                 </div>
-
-
+                <div class="col">
+                    <div class="card">
+                        <img src="https://i.pinimg.com/originals/ee/de/92/eede92cf575d3052ea75f994c8dfefb0.jpg" alt="playing keyboard" class="card-img-top product-img" />
+                        <div class="card-body">
+                            <h2 class="card-title">Keyboards</h2>
+                            <p class="card-text">Browse</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="https://emiliam.com/wp-content/uploads/2019/08/shutterstock_262890266.jpg" alt="playing the ukulele" class="card-img-top product-img" />
+                        <div class="card-body">
+                            <h2 class="card-title">Folk instruments</h2>
+                            <p class="card-text">Browse</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="https://media.npr.org/assets/img/2019/11/11/recorder%20thumb_wide.jpeg?s=1400" alt="playing the recorder" class="card-img-top product-img" />
+                        <div class="card-body">
+                            <h2 class="card-title">Recorders</h2>
+                            <p class="card-text">Browse</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col">
+                    <div class="card">
+                        <img src="https://www.gearank.com/sites/default/files/styles/large/public/modeling-amps-20.jpg?itok=BfC81xl4" alt="amps" class="card-img-top product-img" />
+                        <div class="card-body">
+                            <h2 class="card-title">Amps &amp; Effects</h2>
+                            <p class="card-text">Browse</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="https://images.idgesg.net/images/article/2020/06/fluance-ai60-half-angled-100848061-large.jpg" alt="live sound" class="card-img-top product-img" />
+                        <div class="card-body">
+                            <h2 class="card-title">Live Sound</h2>
+                            <p class="card-text">Browse</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="https://www.larecordingschool.com/wp-content/uploads/2019/03/Avid_C-24_Control_Surfaces-G.jpg" alt="recording gear" class="card-img-top product-img" />
+                        <div class="card-body">
+                            <h2 class="card-title">Recording Gear</h2>
+                            <p class="card-text">Browse</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="https://www.gearank.com/sites/default/files/styles/large/public/acoustic-guitar-cases-gig-bags.jpg?itok=RbLHXka9" alt="accessories" class="card-img-top product-img" />
+                        <div class="card-body">
+                            <h2 class="card-title">Accessories</h2>
+                            <p class="card-text">Browse</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
     )
 }
 export default Shop
