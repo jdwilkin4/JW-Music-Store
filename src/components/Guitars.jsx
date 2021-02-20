80671716ebcff2ad52d8d6fc50ec7f28615f73a1
Jessica Wilkins <jwilkin4@hotmail.com>
2021-02-20 02:44:29 -0800

    removing guitar images

--- src/components/Guitars.jsx
+++ src/components/Guitars.jsx
@@ -9,7 +9,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$499.00</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/lzmu0huullv1why/Guild-Acoustic-Electric.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">Guild OM-240CE Acoustic-Electric</p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -21,7 +21,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$499.00</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/thcpry52hi81q88/G-L-Limited-Edition-Tribute.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">G&amp;L Limited Edition Tribute</p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -33,7 +33,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$99.00</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/l0ols5hsgr2rtge/Rogue-RD80-Dreadnought.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">Rogue RD80 Dreadnought</p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -45,7 +45,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$499.00</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/5hfpozyv0kbwhrc/G-L-Tribute-Legacy-Electric.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">G&amp;L Tribute Legacy Electric</p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -59,7 +59,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$749.99</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/pygfiog6mc7sajw/D%27Angelico-Premier-Series-EXL-1.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">D'Angelico Premier Series EXL-1</p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -71,7 +71,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$199.99</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/ymoa73nfyneq9wq/Black-fender.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">Fender FA-135CE Concert Acoustic-Electric</p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -83,7 +83,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$399.99</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/5r7vvobw2dwoxab/Epiphone.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">Epiphone Limited-Edition 1966 G-400 PRO </p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -95,7 +95,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$809.99</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/o6sy8dhqlriqxjl/Fender-Player-Stratocaster.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">Fender Player Stratocaster HSS  Limited-Edition </p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -109,7 +109,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$199.99</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/bm9xb5hrtnq2zd1/Epiphone-Player-Pack.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">Epiphone Les Paul SL Player Pack</p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -121,7 +121,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$179.99</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/ofeac7uawiyb0ju/Rogue-Pack.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">Rogue Rocketeer Pack</p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -133,7 +133,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$129.99</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/ojxrugixzjqzva0/Rogue-Dreadnot-Pack.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">Rogue RD80PK Dreadnought Pack</p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
@@ -145,7 +145,7 @@ const Guitars = () => {
                     <div className="card">
                         <h2 className="card-header text-center">$219.99</h2>
                         <div className="card-body">
-                            <img src="https://www.dropbox.com/s/srowuk2bjevlx21/Squier-Pack.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                            <img src="" alt="guitar" className="img-thumbnail" />
                             <p className="card-title">Squier Stratocaster Pack</p>
                             <div class="col text-center">
                                 <button className="btn btn-primary">Add to cart</button>
