861d4ccff36c46bb917c0b80cf86f5b4da2fa905
Jessica Wilkins <jwilkin4@hotmail.com>
2021-02-22 05:52:09 -0800

    replacing button with link for details section on guitar page

--- src/components/Guitars.jsx
+++ src/components/Guitars.jsx
@@ -14,7 +14,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/7mlgp00lz2brh4g/D%27Angelico%20Premier%20Series%20EXL-1%20Hollowbody%20Electric%20Guitar%20with%20Stairstep%20Tailpiece%20Transparent%20Wine.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">D'Angelico Premier Series EXL-1</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -29,7 +29,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/qsdmpov3fsktm7l/Fender%20FA-135CE%20Concert%20Acoustic-Electric%20Guitar%20Natural.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Fender FA-135CE Concert Acoustic-Electric</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -44,7 +44,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/bfyaarrvi9d9ai4/Fender%20Player%20Stratocaster%20HSS%20Plus%20Top%20Maple%20Fingerboard%20Limited-Edition%20Electric%20Guitar%20Blue%20Burst.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Fender Player Stratocaster HSS Plus</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -59,7 +59,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/nyw8plnfpwcprwb/Fender%20Player%20Stratocaster%20Maple%20Fingerboard%20Limited%20Edition%20Electric%20Guitar%20Shell%20Pink.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Fender Player Stratocaster Limited Edition</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -76,7 +76,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/0fmpe3qxcxdly2s/Fender%20Player%20Telecaster%20Maple%20Fingerboard%20Electric%20Guitar%20Capri%20Orange.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Fender Player Telecaster Capri Orange</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -91,7 +91,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/2072dvqd552azc4/G%26L%20Limited%20Edition%20Tribute%20ASAT%20Classic%20Ash%20Body%20Electric%20Guitar%20Gloss%20Natural.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">G&amp;L Limited Edition Tribute ASAT Classic</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -106,7 +106,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/wrbd27xyt3pg7xq/Guild%20OM-240CE%20Orchestra%20Acoustic-Electric%20Guitar%20Charcoal%20Burst.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Guild OM-240CE Orchestra Acoustic-Electric</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -121,7 +121,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/6dkq51qxoged7cf/Rogue%20RD80%20Dreadnought%20Acoustic%20Guitar%20Black.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Rogue RD80 Dreadnought Acoustic</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -138,7 +138,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/d3zofp7epfvc84w/Rogue%20Rocketeer%20Electric%20Guitar%20Pack%20Red%20Burst.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Rogue Rocketeer Electric Guitar Pack Black</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -153,7 +153,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/vzcqy4dl9ka9idl/Squier%20Affinity%20Telecaster%20HH%20Electric%20Guitar%20with%20Matching%20Headstock%20Metallic%20Orange.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Squier Affinity Telecaster HH Electric Guitar</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -168,7 +168,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/4wkkat9dimxuare/Squier%20Bullet%20Mustang%20HH%20Limited-Edition%20Electric%20Guitar%20Olympic%20White.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Squier Bullet Mustang HH Limited-Edition</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
@@ -183,7 +183,7 @@ const Guitars = () => {
                                 <img src="https://www.dropbox.com/s/asyss18pudhmwxb/Squier%20Bullet%20Telecaster%20Limited%20Edition%20Electric%20Guitar%20Surf%20Green.jpg?raw=1" alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Squier Bullet Telecaster Limited Edition</p>
                                 <div class="col text-center">
-                                    <button type="button" className="btn product-description btn-link">Details</button>
+                                    <Link className="btn product-description btn-link">Details</Link>
                                 </div>
                                 <div class="col text-center">
                                     <button className="btn btn-primary">Add to cart</button>
