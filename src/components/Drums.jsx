1603b47d44f05b512d843caa734ed07db832fcaa
Jessica Wilkins <jwilkin4@hotmail.com>
2021-02-24 03:52:57 -0800

    replacing images in drums page

--- src/components/Drums.jsx
+++ src/components/Drums.jsx
@@ -12,7 +12,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$89.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/iumjs2gzj0lagn2/Alesis%20CompactKit%204%20Electronic%20Drum%20Kit.jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Alesis CompactKit 4 Electronic Drum Kit.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Alesis CompactKit 4 Electronic Drum Kit</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -27,7 +27,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$899.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/1tvthq95dvr2fse/Alesis%20Crimson%20II%205-Piece%20Electronic%20Drum%20Kit.jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Alesis Crimson II 5-Piece Electronic Drum Kit.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Alesis Crimson II 5-Piece Electronic Drum Kit</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -42,7 +42,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$236.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/dqmhyqkvolxryc2/Boss%20DR-01S%20Rhythm%20Partner%20Drum%20Machine.jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Boss DR-01S Rhythm Partner Drum Machine.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Boss DR-01S Rhythm Partner Drum Machine</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -57,7 +57,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$479.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/7mgogrbpp9bvepx/DW%20Performance%20Series%20Floor%20Tom%20Pewter%20Sparkle%2016%20x%2014%20in..jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/DW Performance Series Floor Tom Pewter Sparkle 16 x 14 in..jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">DW Performance Series Floor Tom Pewter</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -74,7 +74,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$119.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/fhuyqiazpzvyf6n/KAT%20Percussion%20Electronic%20Drum%20and%20Percussion%20Pad%20Sound%20Module.jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/KAT Percussion Electronic Drum and Percussion Pad Sound Module.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">KAT Percussion Electronic Drum</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -89,7 +89,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$29.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/dfzl4hti1abvxrl/Musician%27s%20Gear%20Hickory%20Drum%20Sticks%2010-Pack.jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Musicians Gear Hickory Drum Sticks 10-Pack.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Musician's Gear Hickory Sticks</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -104,7 +104,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$300.00</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/v4pkov1jldyd5n6/Pork%20Pie%20Exotic%20Rosewood%20Zebrawood%20Snare%20Drum%2014%20x%206.5%20in..jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Pork Pie Exotic Rosewood Zebrawood Snare Drum 14 x 6.5 in..jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Pork Pie Exotic Snare</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -119,7 +119,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$349.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/0h75ckk8kwyg6i7/Rogue%205-Piece%20Complete%20Drum%20Set%20Black.jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Rogue 5-Piece Complete Drum Set Black.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Rogue 5-Piece Drum Set</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -136,7 +136,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$514.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/vhnhat3ktwg86b6/Roland%20KD-120%20V-Kick%20Trigger%20Pad%2012%20in..jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Roland KD-120 V-Kick Trigger Pad 12 in..jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Roland KD-120 V-Kick Trigger</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -151,7 +151,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$549.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/h5qe63hs2wfoxmh/Simmons%20SD600%20Electronic%20Drum%20Set%20With%20Mesh%20Heads%20and%20Bluetooth.jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Simmons SD600 Electronic Drum Set With Mesh Heads and Bluetooth.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Simmons SD600 Drum Set</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -166,7 +166,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$672.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/upaa4v7itx4fu09/Traps%20Drums%20A400%20Portable%20Acoustic%20Drum%20Set.jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Traps Drums A400 Portable Acoustic Drum Set.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Traps Drums A400 Portable</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -181,7 +181,7 @@ const Drums = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$119.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/28xjy1p16whrj0c/Yamaha%20Stage%20Custom%20Birch%20Snare%2014x5.5%22%2014%20x%205.5%20in.%20Natural%20Wood.jpg?raw=1" alt="drums" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Yamaha Stage Custom Birch Snare 14x5.5" 14 x 5.5 in. Natural Wood.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Yamaha Stage Custom Snare</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
