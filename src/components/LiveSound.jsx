e361685b174b0f7c286c2d0b2e011c34360f289e
Jessica Wilkins <jwilkin4@hotmail.com>
2021-02-24 02:00:00 -0800

    starting to add products to live sounds page

--- src/components/LiveSound.jsx
+++ src/components/LiveSound.jsx
@@ -1,160 +1,201 @@
 import React from 'react';
+import { Link, withRouter } from "react-router-dom";
+
 
 const LiveSound = () => {
     return (
         <div>
             <h1 className="text-center sub-page-heading">Live Sound</h1>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+            <div className="product-div">
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$149.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/hks5eroh1sehols/Behringer%20XENYX%201002B%205-Channel%20Compact%20Mixer.jpg?raw=1" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title">Behringer XENYX 1002B 5-Channel Compact Mixer</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$99.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/xz3tuu78i343v9c/Digital%20Reference%20DRDK4%204-Piece%20Drum%20Mic%20Kit.jpg?raw=1" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title">Digital Reference DRDK4 4-Piece Drum Mic Kit</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$59.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/ilp4qi3o2zqryir/Digital%20Reference%20DRV200%20Dynamic%20Lead%20Vocal%20Mic.jpg?raw=1" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title">Digital Reference DRV200 Dynamic Lead Vocal Mic</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$</h2>
+                            <div className="card-body">
+                                <img src="" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title"></p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$</h2>
+                            <div className="card-body">
+                                <img src="" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title"></p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$</h2>
+                            <div className="card-body">
+                                <img src="" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title"></p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$</h2>
+                            <div className="card-body">
+                                <img src="" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title"></p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$</h2>
+                            <div className="card-body">
+                                <img src="" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title"></p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$</h2>
+                            <div className="card-body">
+                                <img src="" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title"></p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$</h2>
+                            <div className="card-body">
+                                <img src="" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title"></p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$</h2>
+                            <div className="card-body">
+                                <img src="" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title"></p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$</h2>
+                            <div className="card-body">
+                                <img src="" alt="live sound" className="img-thumbnail" />
+                                <p className="card-title"></p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="live sound" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
         </div>
     )
 }
-export default LiveSound
+export default withRouter(LiveSound) 
