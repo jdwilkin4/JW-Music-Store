050ca6f14a33dcce77c19655160e6c517c4f44f9
Jessica Wilkins <jwilkin4@hotmail.com>
2021-02-24 04:27:07 -0800

    replacing images for guitar page

--- src/components/Guitars.jsx
+++ src/components/Guitars.jsx
@@ -11,7 +11,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$749.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/7mlgp00lz2brh4g/D%27Angelico%20Premier%20Series%20EXL-1%20Hollowbody%20Electric%20Guitar%20with%20Stairstep%20Tailpiece%20Transparent%20Wine.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/DAngelico Premier Series EXL1 Hollowbody Electric Guitar with Stairstep Tailpiece Transparent Wine.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">D'Angelico Premier Series EXL-1</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -26,7 +26,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$199.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/qsdmpov3fsktm7l/Fender%20FA-135CE%20Concert%20Acoustic-Electric%20Guitar%20Natural.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Fender FA135CE Concert Acoustic Electric Guitar Natural.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Fender FA-135CE Concert Acoustic-Electric</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -41,7 +41,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$809.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/bfyaarrvi9d9ai4/Fender%20Player%20Stratocaster%20HSS%20Plus%20Top%20Maple%20Fingerboard%20Limited-Edition%20Electric%20Guitar%20Blue%20Burst.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Fender Player Stratocaster HSS Plus Top Maple Fingerboard Limited-Edition Electric Guitar Blue Burst.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Fender Player Stratocaster HSS Plus</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -56,7 +56,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$774.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/nyw8plnfpwcprwb/Fender%20Player%20Stratocaster%20Maple%20Fingerboard%20Limited%20Edition%20Electric%20Guitar%20Shell%20Pink.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Fender Player Stratocaster Maple Fingerboard Limited Edition Electric Guitar Shell Pink.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Fender Player Stratocaster Limited Edition</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -73,7 +73,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$749.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/0fmpe3qxcxdly2s/Fender%20Player%20Telecaster%20Maple%20Fingerboard%20Electric%20Guitar%20Capri%20Orange.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Fender Player Telecaster Maple Fingerboard Electric Guitar Capri Orange.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Fender Player Telecaster Capri Orange</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -88,7 +88,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$499.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/2072dvqd552azc4/G%26L%20Limited%20Edition%20Tribute%20ASAT%20Classic%20Ash%20Body%20Electric%20Guitar%20Gloss%20Natural.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/G&L Limited Edition Tribute ASAT Classic Ash Body Electric Guitar Gloss Natural.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">G&amp;L Limited Edition Tribute ASAT Classic</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -103,7 +103,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$499.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/wrbd27xyt3pg7xq/Guild%20OM-240CE%20Orchestra%20Acoustic-Electric%20Guitar%20Charcoal%20Burst.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Guild OM-240CE Orchestra Acoustic-Electric Guitar Charcoal Burst.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Guild OM-240CE Orchestra Acoustic-Electric</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -118,7 +118,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$99.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/6dkq51qxoged7cf/Rogue%20RD80%20Dreadnought%20Acoustic%20Guitar%20Black.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Rogue RD80 Dreadnought Acoustic Guitar Black.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Rogue RD80 Dreadnought Acoustic</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -135,7 +135,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$179.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/d3zofp7epfvc84w/Rogue%20Rocketeer%20Electric%20Guitar%20Pack%20Red%20Burst.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Rogue Rocketeer Electric Guitar Pack Red Burst.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Rogue Rocketeer Electric Guitar Pack Black</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -150,7 +150,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$249.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/vzcqy4dl9ka9idl/Squier%20Affinity%20Telecaster%20HH%20Electric%20Guitar%20with%20Matching%20Headstock%20Metallic%20Orange.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Squier Affinity Telecaster HH Electric Guitar with Matching Headstock Metallic Orange.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Squier Affinity Telecaster HH Electric Guitar</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -165,7 +165,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$179.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/4wkkat9dimxuare/Squier%20Bullet%20Mustang%20HH%20Limited-Edition%20Electric%20Guitar%20Olympic%20White.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Squier Bullet Mustang HH Limited-Edition Electric Guitar Olympic White.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Squier Bullet Mustang HH Limited-Edition</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
@@ -180,7 +180,7 @@ const Guitars = () => {
                         <div className="card">
                             <h2 className="card-header text-center">$179.99</h2>
                             <div className="card-body">
-                                <img src="https://www.dropbox.com/s/asyss18pudhmwxb/Squier%20Bullet%20Telecaster%20Limited%20Edition%20Electric%20Guitar%20Surf%20Green.jpg?raw=1" alt="guitar" className="img-thumbnail" />
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Squier Bullet Telecaster Limited Edition Electric Guitar Surf Green.jpg'} alt="guitar" className="img-thumbnail" />
                                 <p className="card-title">Squier Bullet Telecaster Limited Edition</p>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
