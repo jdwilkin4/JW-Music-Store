43945d7ceef409251f4e1a31fbcca9c9db22d571
Jessica Wilkins <jwilkin4@hotmail.com>
2021-02-24 04:59:29 -0800

    adding products to recording page

--- src/components/Recording.jsx
+++ src/components/Recording.jsx
@@ -1,154 +1,194 @@
 import React from 'react';
+import { Link, withRouter } from "react-router-dom";
+
 
 const Recording = () => {
     return (
         <div>
             <h1 className="text-center sub-page-heading">Recording Gear</h1>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+            <div className="product-div">
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$169.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Focusrite Scarlett 2i2 USB Audio Interface (Gen 3).jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Focusrite Scarlett 2i2 USB Audio Interface </p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$229.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Focusrite Scarlett Solo Studio Pack (Gen 3).jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Focusrite Scarlett Solo Studio Pack (Gen 3)</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$199.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Line 6 POD Studio UX2 with POD Farm.jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Line 6 POD Studio UX2 with POD Farm</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$249.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Presonus AudioBox 96 Studio Package.jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Presonus AudioBox 96 Studio Package</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$999.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Rode Complete Podcaster Recording Bundle.jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Rode Complete Podcaster Bundle</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$349.00</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Rode Complete Studio Kit with NT1 Microphone and AI-1 Interface.jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Rode Complete Studio Kit</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$249.00</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/sE Electronics X1S Studio Bundle.jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">sE Electronics X1S Studio Bundle</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$199.00</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/sE Electronics X1S Vocal Pack.jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">sE Electronics X1S Vocal Pack</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$569.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Icon Qcon EX G2 Control Surface.jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Icon Qcon EX G2 Control Surface</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$499.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Shure Choose Your Microphone Bundle SM81.jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Shure Choose Your Mic Bundle</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$899.00</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Universal Audio Apollo Twin X DUO Thunderbolt 3 Audio Interface.jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Universal Audio Apollo Twin X</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="recording" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$219.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Zoom handy video recorder.jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Zoom handy video recorder</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -157,4 +197,4 @@ const Recording = () => {
         </div>
     )
 }
-export default Recording
+export default withRouter(Recording) 
