f9799bae1f16ce936b66c4bdd3d41f52843622c0
Jessica Wilkins <jwilkin4@hotmail.com>
2021-02-24 00:45:01 -0800

    adding products to amps page

--- src/components/Amps.jsx
+++ src/components/Amps.jsx
@@ -1,154 +1,194 @@
 import React from 'react';
+import { Link, withRouter } from "react-router-dom";
+
 
 const Amps = () => {
     return (
         <div>
             <h1 className="text-center sub-page-heading">Amps &amp; Effects</h1>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+            <div className="product-div">
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$1699.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/2enop40xop1v6xz/Ampeg%20SVT-4%20PRO%20Series%20Bass%20Head.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">Ampeg SVT-4 PRO Series</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$354.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/q6gm809dvt474oi/Digitech%20Trio%2B%20Band%20Creator%20Plus%20Looper%20Guitar%20Effects%20Pedal.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">Digitech Trio+ Band Creator</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$119.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/191urr9vyw7kg11/Fender%20RUMBLE%2025%201x8%2025W%20Bass%20Combo%20Amp.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">Fender RUMBLE 25</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$164.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/bnzhhxa53iel8r3/Gator%20GPT-PWR%20Powered%20Pedal%20Tote%20Pedal%20Board%20with%20Carry%20Bag%20Black.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">Gator GPT-PWR Powered Pedal</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$999.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/20m4qls8ckdv0nk/HeadRush%20Pedalboard%20Multi-Effects%20Processor.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">HeadRush Pedalboard Multi-Effects Processor</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$1699.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/jtvsnoq3lxy7epp/Line%206%20Helix%20Multi-Effects%20Guitar%20Pedal.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">Line 6 Helix Multi-Effects Guitar Pedal</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$199.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/szwckdh2ddudba7/LR%20Baggs%20Para%20Acoustic%20Guitar%20DI%20and%20Preamp.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">LR Baggs Para Acoustic Guitar DI and Preamp</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$299.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/hvpg9vp1qhozr1r/LR%20Baggs%20Venue%20DI%20Acoustic%20Guitar%20Direct%20Box%20and%20Preamp.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">LR Baggs Venue DI Acoustic Guitar &amp; Preamp</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$114.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/nydmzkrqp5b66to/Musician%27s%20Gear%20Powered%20Pedal%20Board%20and%20Gig%20Bag.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">Musician's Gear Powered Pedal</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$69.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/8nlwodcvd19kdqr/Pedaltrain%20Nano%20Pedal%20Board%20with%20Soft%20Case%20Black.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">Pedaltrain Nano Pedal Board </p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$44.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/tngcbth0afxpdkx/Rogue%20G10%2010W%201x5%20Guitar%20Combo%20Amp%20Black.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">Rogue G10 10W Amp</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="amps" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$98.99</h2>
+                            <div className="card-body">
+                                <img src="https://www.dropbox.com/s/nmos0ybmbtj7jqo/TC%20Electronic%20Ditto%20Looper%20Guitar%20Effects%20Pedal.jpg?raw=1" alt="amps" className="img-thumbnail" />
+                                <p className="card-title">TC Electronic Ditto Looper</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -157,4 +197,4 @@ const Amps = () => {
         </div>
     )
 }
-export default Amps
+export default withRouter(Amps) 
