14886d56ef73939e8f73ae0f8cb56dedcb429cad
Jessica Wilkins <jwilkin4@hotmail.com>
2021-02-28 04:02:59 -0800

    adding products to classroom page

--- src/components/Classroom.jsx
+++ src/components/Classroom.jsx
@@ -1,154 +1,194 @@
 import React from 'react';
+import { Link, withRouter } from "react-router-dom";
+
 
 const Classroom = () => {
     return (
         <div>
             <h1 className="text-center sub-page-heading">Classroom &amp; Kids</h1>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+            <div className="product-div">
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$21.49</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Deer River Grand Deluxe Choral Folio Black.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Deer River Grand Deluxe Choral Folio Black</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$24.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/First Note 25-Note (G-G) Bell Set with Case.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">First Note 25-Note (G-G) Bell Set with Case</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$8.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Nino 4-Piece Egg Shaker Assortment.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Nino 4-Piece Egg Shaker Assortment</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$150.00</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Remo Kids Percussion Gathering Drum.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Remo Kids Percussion Gathering Drum</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$6.79</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Rhythm Band Claves Select Hardwood.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Rhythm Band Claves </p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$3.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Rhythm Band Cluster Bells.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Rhythm Band Cluster Bells</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$10.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Rhythm Band Copper Cowbell.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Rhythm Band Copper Cowbell</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$419.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Suzuki Alto Xylophone.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Suzuki Alto Xylophone</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$4.49</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Trophy Plastic Maraca Pair.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Trophy Plastic Maraca Pair</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$2.59</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Trophy Triangle 5 in.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Trophy Triangle 5 inches</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$9.49</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Yamaha YRS-20 Soprano Translucent Recorder with Baroque Fingering Translucent Pink.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Yamaha YRS-20 Soprano Recorder</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <h2 className="card-header text-center">$</h2>
-                        <div className="card-body">
-                            <img src="" alt="classroom" className="img-thumbnail" />
-                            <p className="card-title"></p>
-                            <div class="col text-center">
-                                <button className="btn btn-primary">Add to cart</button>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$9.57</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Classroom-images/Yamaha YRS-24B Soprano Recorder with Baroque Fingering.jpg'} alt="classroom" className="img-thumbnail" />
+                                <p className="card-title">Yamaha YRS-24B Soprano Recorder</p>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -157,4 +197,4 @@ const Classroom = () => {
         </div>
     )
 }
-export default Classroom
+export default withRouter(Classroom)
