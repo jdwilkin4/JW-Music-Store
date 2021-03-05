b459973afd424d00c39443e498f68a310be2c768
Jessica Wilkins <jwilkin4@hotmail.com>
2021-03-04 23:28:55 -0800

    adding hot product cards to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import FiveStars from './5Star';
+import { Link, withRouter } from "react-router-dom";
+
 
 function Home() {
     return (
@@ -33,7 +36,88 @@ function Home() {
                     <span class="sr-only">Next</span>
                 </a>
             </div>
+            <h2 className="text-center sub-page-heading">Hot Products</h2>
+            <div className="product-div">
+                <div className="row row-cols-1 row-cols-md-2 g-4">
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$809.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Guitars-images/Fender Player Stratocaster HSS Plus Top Maple Fingerboard Limited-Edition Electric Guitar Blue Burst.jpg'} alt="guitar" className="img-thumbnail" />
+                                <p className="card-title">Fender Player Stratocaster HSS Plus</p>
+                                <FiveStars />
+                                <Link>
+                                    <p className="review-text text-center">(9 Reviews)</p>
+                                </Link>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$229.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Recording-images/Focusrite Scarlett Solo Studio Pack (Gen 3).jpg'} alt="recording" className="img-thumbnail" />
+                                <p className="card-title">Focusrite Scarlett Solo Studio Pack (Gen 3)</p>
+                                <FiveStars />
+                                <Link>
+                                    <p className="review-text text-center">(6 Reviews)</p>
+                                </Link>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$236.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Drums-images/Boss DR-01S Rhythm Partner Drum Machine.jpg'} alt="drums" className="img-thumbnail" />
+                                <p className="card-title">Boss DR-01S Rhythm Partner Drum Machine</p>
+                                <FiveStars />
+                                <Link>
+                                    <p className="review-text text-center">(7 Reviews)</p>
+                                </Link>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <h2 className="card-header text-center">$1,799.99</h2>
+                            <div className="card-body">
+                                <img src={process.env.PUBLIC_URL + '/Keyboards-images/Williams Symphony Grand Digital Piano with Bench Mahogany Red.jpg'} alt="keyboard" className="img-thumbnail" />
+                                <p className="card-title">Williams Symphony Grand Digital Piano</p>
+                                <FiveStars />
+                                <Link>
+                                    <p className="review-text text-center">(9 Reviews)</p>
+                                </Link>
+                                <div class="col text-center">
+                                    <Link className="btn product-description btn-link">Details</Link>
+                                </div>
+                                <div class="col text-center">
+                                    <button className="btn btn-primary">Add to cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
-export default Home
+export default withRouter(Home)
