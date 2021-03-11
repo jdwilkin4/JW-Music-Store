56f491cb86ef49c1f6f968903f03705c667e2e02
Jessica Wilkins <jwilkin4@hotmail.com>
2021-03-10 19:10:35 -0800

    adding star ratings to amps page

--- src/components/Amps.jsx
+++ src/components/Amps.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link, withRouter } from "react-router-dom";
+import ThreeStars from './3Star';
+import FourStars from './4Star';
+import FiveStars from './5Star';
 
 
 const Amps = () => {
@@ -14,6 +17,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/Ampeg SVT-4 PRO Series Bass Head.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">Ampeg SVT-4 PRO Series</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(3 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -29,6 +36,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/Digitech Trio+ Band Creator Plus Looper Guitar Effects Pedal.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">Digitech Trio+ Band Creator</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(4 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -44,6 +55,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/Fender RUMBLE 25 1x8 25W Bass Combo Amp.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">Fender RUMBLE 25</p>
+                                <FiveStars />
+                                <Link>
+                                    <p className="review-text text-center">(7 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -59,6 +74,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/Gator GPT-PWR Powered Pedal Tote Pedal Board with Carry Bag Black.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">Gator GPT-PWR Powered Pedal</p>
+                                <ThreeStars />
+                                <Link>
+                                    <p className="review-text text-center">(5 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -76,6 +95,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/HeadRush Pedalboard Multi-Effects Processor.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">HeadRush Pedalboard Multi-Effects Processor</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(4 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -91,6 +114,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/Line 6 Helix Multi-Effects Guitar Pedal.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">Line 6 Helix Multi-Effects Guitar Pedal</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(3 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -106,6 +133,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/LR Baggs Para Acoustic Guitar DI and Preamp.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">LR Baggs Para Acoustic Guitar DI and Preamp</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(4 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -121,6 +152,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/LR Baggs Venue DI Acoustic Guitar Direct Box and Preamp.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">LR Baggs Venue DI Acoustic Guitar &amp; Preamp</p>
+                                <FiveStars />
+                                <Link>
+                                    <p className="review-text text-center">(7 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -138,6 +173,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/Musicians Gear Powered Pedal Board and Gig Bag.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">Musician's Gear Powered Pedal</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(3 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -153,6 +192,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/Pedaltrain Nano Pedal Board with Soft Case Black.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">Pedaltrain Nano Pedal Board </p>
+                                <FiveStars />
+                                <Link>
+                                    <p className="review-text text-center">(7 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -168,6 +211,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/Rogue G10 10W 1x5 Guitar Combo Amp Black.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">Rogue G10 10W Amp</p>
+                                <ThreeStars />
+                                <Link>
+                                    <p className="review-text text-center">(5 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -183,6 +230,10 @@ const Amps = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Amps-images/TC Electronic Ditto Looper Guitar Effects Pedal.jpg'} alt="amps" className="img-thumbnail" />
                                 <p className="card-title">TC Electronic Ditto Looper</p>
+                                <ThreeStars />
+                                <Link>
+                                    <p className="review-text text-center">(5 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
