7fb4ab02e620a752dec4afd87d49f4e6fce3b135
Jessica Wilkins <jwilkin4@hotmail.com>
2021-03-13 15:58:06 -0800

    adding star ratings to drum page

--- src/components/Drums.jsx
+++ src/components/Drums.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link, withRouter } from "react-router-dom";
+import ThreeStars from './3Star';
+import FourStars from './4Star';
+import FiveStars from './5Star';
 
 
 const Drums = () => {
@@ -14,6 +17,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/Alesis CompactKit 4 Electronic Drum Kit.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Alesis CompactKit 4 Electronic Drum Kit</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(3 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -29,6 +36,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/Alesis Crimson II 5-Piece Electronic Drum Kit.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Alesis Crimson II 5-Piece Electronic Drum Kit</p>
+                                <FiveStars />
+                                <Link>
+                                    <p className="review-text text-center">(7 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -44,6 +55,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/Boss DR-01S Rhythm Partner Drum Machine.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Boss DR-01S Rhythm Partner Drum Machine</p>
+                                <FiveStars />
+                                <Link>
+                                    <p className="review-text text-center">(7 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -59,6 +74,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/DW Performance Series Floor Tom Pewter Sparkle 16 x 14 in..jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">DW Performance Series Floor Tom Pewter</p>
+                                <ThreeStars />
+                                <Link>
+                                    <p className="review-text text-center">(5 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -76,6 +95,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/KAT Percussion Electronic Drum and Percussion Pad Sound Module.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">KAT Percussion Electronic Drum</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(3 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -91,6 +114,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/Musicians Gear Hickory Drum Sticks 10-Pack.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Musician's Gear Hickory Sticks</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(3 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -106,6 +133,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/Pork Pie Exotic Rosewood Zebrawood Snare Drum 14 x 6.5 in..jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Pork Pie Exotic Snare</p>
+                                <ThreeStars />
+                                <Link>
+                                    <p className="review-text text-center">(5 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -121,6 +152,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/Rogue 5-Piece Complete Drum Set Black.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Rogue 5-Piece Drum Set</p>
+                                <FiveStars />
+                                <Link>
+                                    <p className="review-text text-center">(7 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -138,6 +173,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/Roland KD-120 V-Kick Trigger Pad 12 in..jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Roland KD-120 V-Kick Trigger</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(3 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -153,6 +192,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/Simmons SD600 Electronic Drum Set With Mesh Heads and Bluetooth.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Simmons SD600 Drum Set</p>
+                                <ThreeStars />
+                                <Link>
+                                    <p className="review-text text-center">(5 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -168,6 +211,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/Traps Drums A400 Portable Acoustic Drum Set.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Traps Drums A400 Portable</p>
+                                <FourStars />
+                                <Link>
+                                    <p className="review-text text-center">(3 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
@@ -183,6 +230,10 @@ const Drums = () => {
                             <div className="card-body">
                                 <img src={process.env.PUBLIC_URL + '/Drums-images/Yamaha Stage Custom Birch Snare 14x5.5" 14 x 5.5 in. Natural Wood.jpg'} alt="drums" className="img-thumbnail" />
                                 <p className="card-title">Yamaha Stage Custom Snare</p>
+                                <ThreeStars />
+                                <Link>
+                                    <p className="review-text text-center">(5 Reviews)</p>
+                                </Link>
                                 <div class="col text-center">
                                     <Link className="btn product-description btn-link">Details</Link>
                                 </div>
