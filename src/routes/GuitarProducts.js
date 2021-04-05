fc0a18069ebdcde11b6ce0a39e82eee1e918ca4b
Jessica Wilkins <jwilkin4@hotmail.com>
2021-04-05 08:09:48 -0700

    fixing compliation error in guitar project page

--- src/routes/GuitarProducts.js
+++ src/routes/GuitarProducts.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import AngelicoGuitar from '../components/guitar-products/DAngelicoGuitarPage';
+import AngelicoGuitar from '../components/guitar-product-pages/DAngelicoGuitarPage';
 
-const GuitarProducts = () => <Route path="/angelico" exact component={() => <AngelicoGuitar />} />
-
+const GuitarProducts = () => {
+    return (
+        <div>
+            <Route path="/angelico" exact component={() => <AngelicoGuitar />} />
+        </div>
+    )
+}
 
 export default GuitarProducts;
