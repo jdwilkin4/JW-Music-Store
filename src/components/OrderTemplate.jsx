3e73abcaf26156fe10b8c017bc14a10e01ad8bd0
Jessica Wilkins <jwilkin4@hotmail.com>
2021-04-06 13:15:36 -0700

    fixing cart input in order template

--- src/components/OrderTemplate.jsx
+++ src/components/OrderTemplate.jsx
@@ -5,7 +5,23 @@ const OrderTemplate = () => {
         <div>
             <p className="order-msg">Most orders placed before noon ET ship same day (except weekends and holidays).</p>
             <div className="product-row">
-                <input type="number" placeholder="1" min="1" max="100" className="cart-input" />
+                <select className="cart-input">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                </select>
                 <button className="btn btn-primary">Add to cart</button>
             </div>
             <div className="product-icon-col">
