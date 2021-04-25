9336180535826fe8378e4b29a0c6509def62e561
Jessica Wilkins <jwilkin4@hotmail.com>
2021-04-25 11:14:46 -0700

    creating fender telecaster guitar product specs

---
+++ src/components/guitar-product-pages/fender-telecaster-product-specs.js
@@ -0,0 +1,67 @@
+
+//guitar product specs
+const fenderTelecaster = [
+    {
+        title: 'Body',
+        subtitle1: 'Body type:',
+        subtitle2: 'Body material:',
+        subtitle3: 'Body shape:',
+        subtitle4: 'Body wood:',
+        subtitle5: 'Body finish:',
+        subtitle6: 'Orientation:',
+        description1: 'Solid body',
+        description2: 'Solid wood',
+        description3: 'Single cutaway',
+        description4: 'Alder',
+        description5: 'Gloss',
+        description6: 'Right handed'
+    },
+    {
+        title: 'Neck',
+        subtitle1: 'Neck shape:',
+        subtitle2: 'Neck wood:',
+        subtitle3: 'Joint:',
+        subtitle4: 'Scale length:',
+        subtitle5: 'Truss rod:',
+        subtitle6: 'Neck finish:',
+        description1: 'C modern',
+        description2: 'Maple',
+        description3: 'Bolt on',
+        description4: '25.5in',
+        description5: 'Dual-action',
+        description6: 'Satin'
+    },
+    {
+        title: 'Fretboard',
+        subtitle1: 'Material:',
+        subtitle2: 'Radius:',
+        subtitle3: 'Fret size:',
+        subtitle4: 'Number of frets:',
+        subtitle5: 'Inlays:',
+        subtitle6: 'Nut width:',
+        description1: 'Maple',
+        description2: '9.5in',
+        description3: 'Medium jumbo',
+        description4: '22',
+        description5: 'Dot',
+        description6: '1.687 in. (42.8 mm)'
+    },
+    {
+        title: 'Pickups',
+        subtitle1: 'Configuration:',
+        subtitle2: 'Neck:',
+        subtitle3: 'Brand:',
+        subtitle4: 'Active or passive pickups:',
+        subtitle5: 'Series or parallel:',
+        subtitle6: 'Active EQ:',
+        description1: 'SS',
+        description2: 'Player Series single-coil Alnico',
+        description3: 'Fender',
+        description4: 'Passive',
+        description5: 'Parallel',
+        description6: 'No'
+    }
+
+]
+export default fenderTelecaster
+
