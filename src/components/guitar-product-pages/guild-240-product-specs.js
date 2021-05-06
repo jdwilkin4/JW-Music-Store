98fc7c99ef3f42dabcd145bde16223cb7e5ba31b
Jessica Wilkins <jwilkin4@hotmail.com>
2021-05-05 20:40:25 -0700

    adding content to guild 240 product specs

---
+++ src/components/guitar-product-pages/guild-240-product-specs.js
@@ -0,0 +1,65 @@
+//guitar product specs
+const guild240 = [
+    {
+        title: 'Body',
+        subtitle1: 'Body shape:',
+        subtitle2: 'Cutaway:',
+        subtitle3: 'Back and sides:',
+        subtitle4: 'Top wood:',
+        subtitle5: 'Bracing pattern:',
+        subtitle6: 'Orientation:',
+        description1: 'Orchestra/OM',
+        description2: 'Single',
+        description3: 'Mahogany',
+        description4: 'Spruce',
+        description5: 'Scalloped X',
+        description6: 'Right handed'
+    },
+    {
+        title: 'Neck',
+        subtitle1: 'Neck shape:',
+        subtitle2: 'Neck wood:',
+        subtitle3: 'Nut width:',
+        subtitle4: 'Fingerboard:',
+        subtitle5: 'Scale length:',
+        subtitle6: 'Number of frets:',
+        description1: 'C modern',
+        description2: 'Mahogany',
+        description3: '1.69" (43 mm)',
+        description4: 'Pao Ferro',
+        description5: '25.5"',
+        description6: '20'
+    },
+    {
+        title: 'Electronics',
+        subtitle1: 'Pickup/preamp:',
+        subtitle2: 'Brand:',
+        subtitle3: 'Configuration:',
+        subtitle4: 'Preamp EQ:',
+        subtitle5: 'Feedback filter:',
+        subtitle6: 'Tuner:',
+        description1: 'yes',
+        description2: 'Guild',
+        description3: 'Undersaddle pickup',
+        description4: '2-band',
+        description5: 'No',
+        description6: 'No'
+    },
+    {
+        title: 'Other',
+        subtitle1: 'Headstock overlay:',
+        subtitle2: 'Tuning:',
+        subtitle3: 'Bridge:',
+        subtitle4: 'Saddle and nut:',
+        subtitle5: 'Number of strings:',
+        subtitle6: 'Accessories:',
+        description1: 'Not specified',
+        description2: 'Guild Vintage-Style open gear',
+        description3: 'Pau ferro',
+        description4: 'Bone',
+        description5: '6',
+        description6: 'None'
+    }
+
+]
+export default guild240
