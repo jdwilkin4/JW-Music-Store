4cceb771274abba37fc916eecc9a199241f66887
Jessica Wilkins <jwilkin4@hotmail.com>
2021-05-17 23:29:21 -0700

    styling helpbot container

--- src/components/HelpBot.jsx
+++ src/components/HelpBot.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+
+
 
 const HelpBot = () => {
+    const theme = {
+        background: '#f5f8fb',
+        fontFamily: 'Sans Serif',
+        headerBgColor: '#043352',
+        headerFontColor: 'white',
+        headerFontSize: '2rem',
+        botBubbleColor: '#5a84cd',
+        botFontColor: 'white',
+        userBubbleColor: '#fff',
+        userFontColor: '#4a4a4a',
+    }
+
+    const steps = [
+        {
+            id: '1',
+            message: 'Welcome to JW Music Store!',
+            end: true,
+        },
+    ];
     return (
         <div className="help-bot">
-            <i className="fa fa-question-circle" aria-hidden="true"></i>
-            <p className="help-bot-text">Help</p>
+            <ThemeProvider theme={theme}>
+                <ChatBot steps={steps} />
+            </ThemeProvider>
         </div>
 
     )
