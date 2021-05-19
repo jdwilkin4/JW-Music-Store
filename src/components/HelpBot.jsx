e8dc55fe89798dd790894d85c58929ab2a2826d8
Jessica Wilkins <jwilkin4@hotmail.com>
2021-05-18 22:15:13 -0700

    adding condition to remove chatbot on mobile screens

--- src/components/HelpBot.jsx
+++ src/components/HelpBot.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ChatBot from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
 
 
 
 const HelpBot = () => {
+
+    //only show chatbot on ipads and desktops
+    const [largeScreen, setLargeScreen] = useState(window.innerWidth > 700);
+
+    const showBot = () => {
+        setLargeScreen(window.innerWidth > 700);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", showBot);
+        return () => window.removeEventListener("resize", showBot);
+    });
+
+    const config = {
+        width: "300px",
+        height: "300px",
+        floating: true,
+        headerTitle: 'JW Music Store Chat'
+    };
+
     const theme = {
         background: '#f5f8fb',
         fontFamily: 'Sans Serif',
         headerBgColor: '#043352',
         headerFontColor: 'white',
-        headerFontSize: '2rem',
+        headerFontSize: '1.5rem',
         botBubbleColor: '#5a84cd',
         botFontColor: 'white',
         userBubbleColor: '#fff',
@@ -21,14 +41,29 @@ const HelpBot = () => {
         {
             id: '1',
             message: 'Welcome to JW Music Store!',
-            end: true,
+            trigger: '2',
         },
+        {
+            id: '2',
+            delay: 1000,
+            message: 'My name is JW Bot. How may I help you today?',
+            end: true
+        }
     ];
     return (
         <div className="help-bot">
-            <ThemeProvider theme={theme}>
-                <ChatBot steps={steps} />
-            </ThemeProvider>
+            {
+                largeScreen ? (
+                    <ThemeProvider theme={theme}>
+                        <ChatBot
+                            steps={steps}
+                            {...config}
+                        />
+                    </ThemeProvider>
+                ) : (
+                        <div></div>
+                    )
+            }
         </div>
 
     )
