8de633e5ff05f7ef12c4dbe25b50a3b15df86f66
Jessica Wilkins <jwilkin4@hotmail.com>
2021-05-19 19:18:14 -0700

    working on intro for chatbot

--- src/components/helpbot/config.js
+++ src/components/helpbot/config.js
@@ -1,6 +1,11 @@
 const Config = {
     botAvatar: "/color-music-note.jpg",
-    width: "300px",
+    bubbleStyle: {
+        color: 'white',
+        fontSize: '1.1rem',
+        backgroundColor: '#043352'
+    },
+    width: "600px",
     height: "400px",
     floating: true,
     headerTitle: 'JW Music Store Chat'
--- src/components/helpbot/steps.js
+++ src/components/helpbot/steps.js
@@ -1,17 +1,37 @@
+const productCategories = [
+    ''
+]
+
+
+
 const steps = [
     {
         id: '1',
-        message: 'Hi, my name is JW Bot! What is your name?',
+        delay: 1500,
+        message: 'Hi, my name is JW Bot! How may I help you today?',
         trigger: '2',
     },
     {
         id: '2',
-        user: true,
-        trigger: '3'
+        options: [
+            { value: 1, label: 'Product recommendations', trigger: '3' },
+            //{ value: 2, label: 'FAQ', trigger: '6' },
+            //{ value: 3, label: 'Store hours', trigger: '7' },
+        ],
     },
     {
         id: '3',
-        message: 'Hi {previousValue}! How may I help you today?',
+        message: 'What instrument do you play?',
+        trigger: '4'
+
+    },
+    {
+        id: '4',
+        user: true,
+        trigger: '5'
+    }, {
+        id: '5',
+        message: 'Here is what I would suggest',
         end: true
     }
 ];
